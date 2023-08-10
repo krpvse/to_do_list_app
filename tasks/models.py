a833fce394969c67347b7633e5c12a5ed2b30286
krpvby <138283843+krpvby@users.noreply.github.com>
2023-08-10 19:56:19 +0300

    add tasks app: create html and develop app functions

---
+++ tasks/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=42, verbose_name='Заголовок')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+    task_type = models.CharField(max_length=10, verbose_name='Тип задачи')
+    is_completed = models.BooleanField(default=False, verbose_name='Выполнена')
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return f'Задача: {self.title} | Тип задачи: {self.task_type}'
--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'users',
+    'tasks',
 ]
 
 MIDDLEWARE = [
