0965a601e79ad33f1f99acb4d19332f0da06f41f
Sergey Karpov <krpvby@gmail.com>
2023-11-11 10:04:44 +0300

    add docker functionality

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'to_do_list.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv('DB_NAME'),
-        "USER": os.getenv('USER'),
-        "PASSWORD": os.getenv('PASSWORD'),
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "NAME": os.getenv('POSTGRES_DB'),
+        "USER": os.getenv('POSTGRES_USER'),
+        "PASSWORD": os.getenv('POSTGRES_PASSWORD'),
+        "HOST": os.getenv('POSTGRES_HOST'),
+        "PORT": os.getenv('POSTGRES_PORT'),
     }
 }
 
